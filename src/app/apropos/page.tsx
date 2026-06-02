608609ef1da688f2046e8d639f9b51603c2c7706
Jules TADEJ <contact@fromagersdefrance.com>
2026-06-02 09:27:47 +0200

    separation Hero (/) et APropos (/apropos) en pages distinctes

---
+++ src/app/apropos/page.tsx
@@ -0,0 +1,5 @@
+import APropos from "@/components/sections/APropos";
+
+export default function AProposPage() {
+  return <APropos />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import Hero from "@/components/sections/Hero";
-import APropos from "@/components/sections/APropos";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <APropos />
-    </>
-  );
+  return <Hero />;
 }
